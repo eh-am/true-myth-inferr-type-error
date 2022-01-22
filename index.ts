497f2acae567cf2b341a3047a84db6614f0f45ec
eduardo <eduardoaleixomartins@gmail.com>
2022-01-22 20:47:44 -0300

    result issue

--- index.ts
+++ index.ts
@@ -1,15 +1,8 @@
-import Maybe from "true-myth/maybe";
+import Result from "true-myth/result";
 
-let m = Maybe.of("Hello!");
-
-//if (m.isJust) {
-//  m.value;
-//} else {
-//  //  m.value;
-//}
-
-if (m.isNothing) {
-  //  m.value;
-} else {
-  m.value;
+function mightSucceed(doesSucceed: boolean): Result<number, string> {
+  return doesSucceed ? Result.ok(42) : Result.err("something went wrong!");
 }
+
+const a = mightSucceed(false);
+a.value; // should've errored
